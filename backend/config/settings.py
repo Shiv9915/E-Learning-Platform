be1665722deeb48630ad36b309d661e3545f1a46
Shiv9915 <ss9981720@gmail.com>
2026-08-02 03:43:12 +0530

    Configure production settings for Render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-agxbzb#1r33p7^^bg0x+7^n0g#cqr(*l!4i$hp(+ks&^!oe_-u"
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "django-insecure-agxbzb#1r33p7^^bg0x+7^n0g#cqr(*l!4i$hp(+ks&^!oe_-u",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
